69ab2c25b60909fef9eb3d4675c4bef4484bb6cc
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-10-29 17:28:37 -0400

    chore(config): add eslint & prettier config files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+};
