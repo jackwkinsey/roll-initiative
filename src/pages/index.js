e1674121ebb0dfd4bfd4565c5cb9506c5f9f254c
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-10-31 14:09:42 -0400

    refactor: move add character form to its own component (#4)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,7 @@
-import { Component } from 'react';
+import AddCharacterForm from '../components/AddCharacterForm';
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { name: '', initiative: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event, fieldName) {
-    this.setState({ [fieldName]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    // eslint-disable-next-line no-alert
-    alert(`Entered character data: ${JSON.stringify(this.state)}`);
-    event.preventDefault();
-  }
-
-  render() {
-    const { name, initiative } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name-input">
-          Name:
-          <input
-            value={name}
-            type="text"
-            name="name"
-            id="name-input"
-            onChange={event => this.handleChange(event, 'name')}
-          />
-        </label>
-        <label htmlFor="initiative-input">
-          Initiative:
-          <input
-            value={initiative}
-            type="number"
-            min="1"
-            max="50"
-            name="initiative"
-            id="initiative-input"
-            onChange={event => this.handleChange(event, 'initiative')}
-          />
-        </label>
-        <input type="submit" value="Add Character" />
-      </form>
-    );
-  }
-}
+const HomePage = () => {
+  return <AddCharacterForm />;
+};
 
 export default HomePage;
