bbc643ae439ca7304db0cb2606b9126a7948980d
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-11-01 00:04:55 -0400

    Add new queue data structures to maintain combat list state (#8)
    * chore(config): ignore some eslint rules
    
    * chore(config): setup jest and babel config
    
    * chore(deps): add jest and babel dev packages
    
    * feat: add Queue and BattleQueue data structures
    
    * refactor: rename add method so it doesn't overwrite base class add()
    
    * feat: add End Turn button for first character in list
    
    * feat: uses new BattleQueue structure to maintain state
    
    * chore: use babelrc instead of babel config file
    
    * chore(deps): remove unneeded babel packages

--- src/components/CombatList/CombatList.js
+++ src/components/CombatList/CombatList.js
@@ -1,13 +1,19 @@
 const CombatList = props => {
   const { characters } = props;
-  const Characters = characters.map(character => {
+  const Characters = characters.map((character, index) => {
     const { name, initiative } = character;
     return (
       <div key={name}>
-        {name} - {initiative}
+        {name} - {initiative}{' '}
+        {!index ? (
+          <button type="button" onClick={props.endTurn}>
+            End Turn
+          </button>
+        ) : null}
       </div>
     );
   });
+
   return <>{Characters}</>;
 };
 
